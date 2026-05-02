5a8037b63bfd5e78c655e2f21c4646f38c6b51e8
isaac gatheru <igatheru7@gmail.com>
2026-05-02 15:19:03 +0300

    vercel config fix test 2

--- spa-17/vite.config.js
+++ spa-17/vite.config.js
@@ -4,6 +4,10 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+    // Ensure this is NOT set to something else, or if it is, 
+    // update Vercel to match this name.
+    outDir: 'dist', },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
